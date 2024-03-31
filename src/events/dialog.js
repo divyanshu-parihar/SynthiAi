49f820ed6583c59965146086fd26bdea4c957fce
Divyanshu Parihar <divyanshu1447@gmail.com>
2024-03-31 17:04:49 +0530

    dialog next and back

--- src/events/dialog.js
+++ src/events/dialog.js
@@ -2,7 +2,7 @@ const { fileFromPath } = require("openai");
 const { prisma } = require("../handler/database");
 const { Markup } = require("telegraf");
 async function dialog(ctx) {
-  let historyIndex = ctx.match[0].split("-")[1];
+  let historyIndex = parseInt(ctx.match[0].split("-")[1]);
   // let historyIndex = 0;
   const messageId = ctx.update.callback_query.message.message_id;
 
@@ -27,42 +27,29 @@ async function dialog(ctx) {
 
     historyIndex += 5;
 
-    await ctx.editMessageText(`Here's your 🗂️ Dialog History
-  You can select any dialog and continue it!
-  `);
+    await ctx.editMessageText(
+      `Here's your 🗂️ Dialog History\nYou can select any dialog and continue it!`
+    );
     console.log(keyboard);
-
-    if (filtered.length > historyIndex + 5) {
+    console.log("index", historyIndex);
+    if (data.length >= parseInt(historyIndex)) {
       keyboard.push([
-        { text: "Next", callback_data: "Dialog-" + historyIndex },
+        { text: "Next", callback_data: "Dialog-" + parseInt(historyIndex) },
       ]);
     }
-
-    if (newLoc < config.profiles.length) {
+    console.log("history index", historyIndex);
+    if (historyIndex - 10 >= 0) {
       keyboard.push([
-        { text: "Next", callback_data: "next-" + parseInt(newLoc) },
+        {
+          text: "Back",
+          callback_data: "Dialog-" + parseInt(historyIndex - 10),
+        },
       ]);
     }
     keyboard.push([{ text: "Back to menu", callback_data: "BackMenu" }]);
 
     await ctx.editMessageReplyMarkup(
-      Markup.inlineKeyboard([
-        // [{ text: "Assitant", callback_data: "Assitant" }],
-        // [{ text: "Brief Assitant", callback_data: "BriefAssitant" }],
-        // [{ text: "Code Developer", callback_data: "CodeDeveloper" }],
-        // [{ text: "DALLE-3 Image Generation", callback_data: "Dalle" }],
-        // [{ text: "Eva Elfie(18+) [PRO]", callback_data: "EvaElfie" }],
-        ...keyboard,
-        // [[{ text: "Dialog History", callback_data: "Dialog-" + historyIndex }]][
-
-        // [{ text: "Delete all dialogs", callback_data: "DeleteAllDialogs" }],
-        // // [{ text: "Next", callback_data: "Dialog-" + historyIndex }],
-
-        // [
-        //   // ],
-        //   { text: "Back to menu", callback_data: "BackMenu" },
-        // ],
-      ]).reply_markup,
+      Markup.inlineKeyboard([...keyboard]).reply_markup,
       { message_id: messageId }
     );
   } catch (e) {
