b6f4ed5f216382774daced9b23849c144549a5cf
Divyanshu Parihar <divyanshu1447@gmail.com>
2024-04-08 17:05:42 +0530

    updated webhooks: :

--- src/webhook/index.js
+++ src/webhook/index.js
@@ -3,56 +3,58 @@ const app = express();
 const axios = require("axios");
 const fs = require("fs");
 const { prisma } = require("../handler/database");
-
 app.use(express.json());
 // const { PrismaClient } = require("@prisma/client");
 // const prisma = new PrismaClient();
 app.post("/webhook", async (req, res) => {
-  const data = prisma.userPurchasedToken.findMany();
+  // const data = prisma.userPurchasedToken.findMany();
 
   try {
-    // const payload = req.body.event.data; // Extracting the data object from the payload
+    const payload = req.body.event.data; // Extracting the data object from the payload
     // const chargeId = payload.id;
-    // const status = payload.timeline[payload.timeline.length - 1].status; // Getting the latest status from timeline
-    // const amount = parseFloat(payload.pricing.local.amount);
+    const status = payload.timeline[payload.timeline.length - 1].status; // Getting the latest status from timeline
+    const amount = parseInt(payload.pricing.local.amount);
     // const currency = payload.pricing.local.currency;
     // const customerName = payload.metadata.name;
     // const customerEmail = payload.metadata.email;
 
-    if (req.body.event.event.type == "charge:created") {
+    if (status == "COMPLETED") {
       const data = await prisma.userOrders.findFirst({
         where: {
           orderid: req.body.event.data.id,
         },
       });
-
+      // url: 'https://commerce.coinbase.com/pay/3592c237-e72b-4e71-8de2-569dcf0b515f'
       const user = data.userid;
 
       const dataPoint = await prisma.userPurchasedToken.findUnique({
         where: { userid: user },
       });
-
+      console.log(data, user, dataPoint);
       if (!dataPoint) {
         throw new Error("Data point not found");
       }
 
       // Calculate the new value
-      const currentValue = dataPoint.value;
-      const newValue = currentValue + incrementAmount;
+      const currentValue = dataPoint.token;
+      const newValue = currentValue + amount * 100;
 
       // Update the database with the new value
+      console.log(newValue);
       await prisma.userPurchasedToken.update({
         where: { userid: user },
         data: {
-          token: newValue,
+          token: parseInt(newValue),
         },
       });
+
+      return res.send(JSON.stringify(data));
     }
   } catch (e) {
-    console.log("error happened for ", data);
+    // console.log("error happened for ", user, currentValue, newValue);
+    console.log(e);
+    return res.send(JSON.stringify(e));
   }
-
-  return res.send(JSON.stringify(data));
 });
 
 app.post("/create", async (req, res) => {
@@ -85,7 +87,7 @@ app.post("/create", async (req, res) => {
       await prisma.userOrders.create({
         data: {
           userid: data.userid,
-          orderid: result.data.data.hosted_url,
+          orderid: result.data.data.id,
         },
       });
     } catch (e) {
